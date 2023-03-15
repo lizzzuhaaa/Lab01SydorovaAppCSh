55df28cc2df1d88ad75e98b93c2debe54ca94a4f
lizuwka <mur.mur.may.li@gmail.com>
2023-03-15 10:21:28 +0200

    Lab02

---
+++ WpfApp1/Validation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01Sydorova
+{
+    internal class Validation: Exception
+    {
+        private string _message; 
+
+        public Validation(string message) 
+        {
+            this._message = message;
+        }
+        public Validation(string message, DateTime date) : this("Invalid Date due to incorrect age")
+        {
+        }
+        public Validation(string message, string email) : this("Your email is invalid format")
+        {
+        }
+
+        public override string Message 
+        {
+            get { return _message; }
+        }
+    }
+}
